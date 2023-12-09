f21a1542df06d9d32df0780dfb5d52994a099793
azz <4zv4l@protonmail.com>
2023-12-09 20:26:40 +0800

    Changed the read stdin non async to async and in another function use array instead of vector (no allocation required) and outside of the loop

--- sjirc/client/src/main.rs
+++ sjirc/client/src/main.rs
@@ -1,49 +1,44 @@
 use std::error::Error;
-use std::io;
-use std::io::stdin;
-use tokio::io::AsyncWriteExt;
+use std::io::{stdout,Write};
+use tokio::io::{AsyncReadExt, AsyncWriteExt, BufReader, AsyncBufReadExt, ReadHalf, stdin};
 use tokio::net::TcpStream;
+
+async fn read_from_server(mut reader: ReadHalf<TcpStream>) -> Result<(), Box<dyn Error>> {
+    let mut msg = [0;1024];
+    loop {
+        match reader.read(&mut msg).await {
+            Ok(n) => {
+                let message = String::from_utf8_lossy(&msg[0..n]);
+                print!("{}", message);
+                stdout().flush().expect("couldnt flush stdout");
+            }
+            Err(e) => return Err(Box::new(e)),
+        }
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = std::env::args().collect();
     if args.len() != 3 {
         println!("usage: {} [ip] [port]", args[0]);
+        return Ok(())
     }
     let (ip, port) = (&args[1], &args[2]);
     println!("Listening on {ip}:{port}");
 
-    let mut stream = TcpStream::connect(format!("{ip}:{port}")).await?;
+    let stream = TcpStream::connect(format!("{ip}:{port}")).await?;
+    let (reader, mut writer) = tokio::io::split(stream);
 
-    //stream.write_all(b"/NICKNAME").await?;
-    //stream.write_all(b"hello world!").await?;
+    tokio::spawn(async move {
+        let _ = read_from_server(reader).await;
+    });
 
-    //Ok(())
-
+    let mut user_input = String::new();
+    let mut stdin_reader = BufReader::new(stdin());
     loop {
-        // Wait for the socket to be readable
-        stream.readable().await?;
-        let mut msg = vec![0; 1024];
-        // Try to read data, this may still fail with `WouldBlock`
-        // if the readiness event is a false positive.
-        match stream.try_read(&mut msg) {
-            Ok(n) => {
-                msg.truncate(n);
-                let string = String::from_utf8(msg).expect("Our bytes should be valid utf8");
-                println!("{:?}", string);
-                let mut s = String::new();
-                stdin()
-                    .read_line(&mut s)
-                    .expect("Did not enter a correct string");
-                stream.write_all(s.as_bytes()).await?;
-                //break;
-            }
-            Err(ref e) if e.kind() == io::ErrorKind::WouldBlock => {
-                continue;
-            }
-            Err(e) => {
-                return Err(e.into());
-            }
-        }
+        stdin_reader.read_line(&mut user_input).await.expect("couldnt read line from user");
+        writer.write_all(user_input.as_bytes()).await?;
+        user_input.clear();
     }
 }
